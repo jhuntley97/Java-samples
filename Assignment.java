ea50a6dcf4a91543fb7d6a889648c43dabcb3008
jhuntley97 <jhuntley@my.nl.edu>
2019-05-14 15:01:15 -0500

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,16 @@
+
+
+
+class Assignment
+{
+	public static void main(String [] args){
+		String FirstName = "Jwaun";
+		String LastName = "Huntley";
+		System.out.println(FirstName + LastName);
+	
+	
+	} 
+  
+  
+}
+  
